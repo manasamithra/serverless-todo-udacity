9d009dd52f6c9bd4a71dafa7fe80c7f306076711
manasamithra <63588983+manasamithra@users.noreply.github.com>
2021-06-17 18:03:25 +0530

    Add files via upload

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = '1yvyq9nmwh'
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'manasa-dev.us.auth0.com',           // Auth0 domain
+  clientId: 'Ja4R68iDVkSxyQEGPyRBDSX7722VQbm8',  // Auth0 client id
+  callbackUrl: 'http://localhost:3000/callback'
+}
